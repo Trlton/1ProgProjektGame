14c651e02016ef591be5e2475d97a83adba5a7e6
Rasmus Krogsgaard Vester <124626087+Trlton@users.noreply.github.com>
2024-05-19 22:29:43 +0200

    Added colors, move bindings, game title and some player values

--- constants.py
+++ constants.py
@@ -1,6 +1,30 @@
+import pygame.event
+
 FPS = 60
 
-SCREENHEIGHT = 1080
-SCREENWITDH = 1920
+SCREEN_HEIGHT = 1080
+SCREEN_WIDTH = 1920
 
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (200, 200, 200)
 RED = (255,0,0)
+
+GAME_TITLE = "__main__"
+
+#PLAYER values
+
+playerMaxSpeed = 10
+playerMaxHealth = 10
+
+
+    #Control bindings
+        #Movement
+upBinding = pygame.K_w
+downBinding = pygame.K_s
+leftBinding = pygame.K_a
+rightBinding = pygame.K_d
+
+
+diagonal_movement_multiplier = 0,71
