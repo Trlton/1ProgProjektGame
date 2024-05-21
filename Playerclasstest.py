09cf3a1dfd48939136317db854ac5d6d56852d08
Fredehjort <at112384@edu.aarhustech.dk>
2024-05-21 09:40:42 +0200

    Uploadede den forkerte ting

---
+++ Playerclasstest.py
@@ -0,0 +1,59 @@
+import pygame
+from pygame.math import Vector2
+import constants as game_constants
+#bør virke
+class gameObject:
+    xSpeed = 0
+    ySpeed = 0
+    widthHitbox = 0
+    heightHitbox = 0
+    maxHealth = 0
+    currentHealth = 0
+
+    def healthChange(self, maxHealth, currentHealth, damage):
+        # Damages or heals entity based on how much damage the damage source deals.
+        # Negative damage = healing
+        currentHealth -= damage
+        print(currentHealth)
+        if currentHealth > maxHealth:
+            currentHealth = maxHealth
+
+
+class player(gameObject):
+    def __init__(self, screen, xpos, ypos, width, height):
+        super().__init__()
+        self.rect = pygame.Rect(width, height)
+        self.xplayer = xpos
+        self.yplayer = ypos
+        self.pos = Vector2(xpos, ypos)
+        self.vel = Vector2(0, 0)
+        self.acc = Vector2(0, 0)
+        self.currentHealth = game_constants.playerMaxHealth
+        self.screen = screen
+
+    def updatePlayer(self):
+        self.acc = Vector2(0, 0)
+
+        # Check input and set acceleration
+        keys = pygame.key.get_pressed()
+        if keys[game_constants.leftBinding]:
+            self.acc.x = -1
+        if keys[game_constants.rightBinding]:
+            self.acc.x = 1
+        if keys[game_constants.upBinding]:
+            self.acc.y = -1
+        if keys[game_constants.downBinding]:
+            self.acc.y = 1
+
+        if self.acc.length_squared() > 0:
+            self.acc = self.acc.normalize()
+
+        self.acc *= game_constants.playerMaxSpeed
+        self.pos += self.acc
+
+        # Update player position
+        self.rect.center = self.pos
+
+    def draw(self, screen, hitboxColor, camera):
+        adjusted_rect = self.rect.move(camera.camera.topleft)
+        pygame.draw.rect(screen, hitboxColor, adjusted_rect)
