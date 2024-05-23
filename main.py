5a7195dce583856e00ec31ab1db7278c95e33d76
Rasmus Krogsgaard Vester <124626087+Trlton@users.noreply.github.com>
2024-05-23 10:02:57 +0200

    updated plater starting position to the middle

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from cameraClass import Camera
 pygame.init()
 
 # setting up the display
-gameScreen = pygame.display.set_mode((game_constants.SCREEN_WIDTH, game_constants.SCREEN_HEIGHT))
+gameScreen = pygame.display.set_mode((game_constants.resolution))
 pygame.display.set_caption(game_constants.GAME_TITLE)
 
 # Create clock for fps controll for ease of use
@@ -17,15 +17,15 @@ clock = pygame.time.Clock()
 
 # use player object
 player_object = player(gameScreen,
-                       game_constants.SCREEN_WIDTH // 2,
-                       game_constants.SCREEN_HEIGHT // 2,
+                       game_constants.GAME_WIDTH // 2,
+                       game_constants.GAME_HEIGHT // 2,
                        game_constants.playerHitboxWidth,
                        game_constants.playerHitboxHeight)
 
 # make camera obejct:
                     # values are witdh and height of playspace
-camera = Camera(game_constants.gameWidth,
-                game_constants.gameHeight)
+camera = Camera(game_constants.GAME_WIDTH,
+                game_constants.GAME_HEIGHT)
 
 # testing grounds terrain - checkered playground
 def draw_checkered_background(surface, block_size, camera):
@@ -44,38 +44,18 @@ while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-
     # Update player
     player_object.updatePlayer()
 
-    # Update camera
+    # Update camera onto player
     camera.update(player_object)
 
-    # draw functions
+    # draw functions stuff
     gameScreen.fill(game_constants.BLACK)
-    draw_checkered_background(gameScreen, 50, camera)
+    draw_checkered_background(gameScreen, 100, camera)
     player_object.draw(gameScreen, game_constants.RED, camera)
 
-    # Update the display
-    pygame.display.flip()
-
-    # Cap the frame rate
-    clock.tick(game_constants.FPS)
-
-pygame.quit()
-
-    # Update player
-    player_object.updatePlayer()
-
-    # Update camera
-    camera.update(player_object)
-
-    # draw functions
-    gameScreen.fill(game_constants.BLACK)
-    draw_checkered_background(gameScreen, 50, camera)
-    player_object.draw(gameScreen, game_constants.RED, camera)
-
-    # Update the display
+    # update display
     pygame.display.flip()
 
     # Cap the frame rate
